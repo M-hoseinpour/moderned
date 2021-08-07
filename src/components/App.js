1dbde7ea497482f44f38f73654062380105adfb7
Mohammad Hosein Pour <mostafahoseinpour.mhp@gmail.com>
2021-08-07 13:56:14 +0430

    loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 import { createBrowserHistory } from 'history'
@@ -11,13 +11,38 @@ import Meet from './meet/Meet';
 import Carousel_custom from './carousel/Carousel';
 import Footer from './footer/Footer';
 import Login from './login/Login';
-
+import { IMAGES } from "./Images"
 
 
 
 const newHistory = createBrowserHistory();
 
 function App() {
+  
+  const [imgsLoaded, setImgsLoaded] = useState(false)
+
+  useEffect(() => {
+    const loadImage = image => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image()
+        loadImg.src = image.url
+    
+        loadImg.onload = () =>
+          resolve(image.url)
+            
+        loadImg.onerror = err => reject(err)
+      })
+    }
+
+    Promise.all(IMAGES.map(image => loadImage(image)))
+      .then(() => setImgsLoaded(true))
+      .catch(err => console.log("Failed to load images", err))
+  }, [])
+  
+  
+  
+  
+  
   return (
         <Router history={newHistory}>
         <div className="App">
@@ -26,16 +51,24 @@ function App() {
             <Route path='/login'>
               <Login />
             </Route>
-            
+
             <Route path='/' exact>
-              <Header />
-              <Comment />
-              <About />
-              <Meet />
-              <Introduction />
-              <News />
-              <Carousel_custom />
-              <Footer />
+            {imgsLoaded ? (
+              <div>
+                  <Header />
+                  <Comment />
+                  <About />
+                  <Meet />
+                  <Introduction />
+                  <News />
+                  <Carousel_custom />
+                  <Footer />
+              </div>
+              ) : (
+                <div className='loading'>
+                  <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+                </div>
+                )}
             </Route>
           </Switch>
         </div>
---
+++ src/components/Images.js
@@ -0,0 +1,17 @@
+export const IMAGES = [
+    {
+      id: 0,
+      url:
+        "https://images.adsttc.com/media/images/54b4/9502/e58e/ce52/8e00/0197/slideshow/%E5%A4%96%E8%A6%B3%E5%A4%95%E6%99%AF4.jpg?1421120756"
+    },
+    {
+      id: 1,
+      url:
+        "https://img.wallpapersafari.com/desktop/1920/1080/64/44/cL6ZFH.jpg"
+    },
+    {
+      id: 2,
+      url:
+        "https://www.kazakov.de/wp-content/uploads/2019/01/heydar-aliyev-center-07.jpg"
+    },
+  ]
